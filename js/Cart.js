7bb8e1f268d51fcd2205175380a9839e24c3b31c
Mohamed Salah <m.salah532@gmail.com>
2023-12-13 02:01:32 +0200

    Update Style and Format

--- js/Cart.js
+++ js/Cart.js
@@ -5,178 +5,175 @@ var cartitems = JSON.parse(arrayString);
 window.onload = checkArray;
 
 function checkArray() {
-    if (cartitems.length === 0) {
-        document.getElementById("contentCart").style.display = "block";
-        document.getElementById("cart").style.display = "none";
-    } else if (cartitems.length > 0) {
-        document.getElementById("contentCart").style.display = "none";
-        document.getElementById("cart").style.display = "block";
-    }
+  if (cartitems.length === 0) {
+    document.getElementById("contentCart").style.display = "block";
+    document.getElementById("cart").style.display = "none";
+  } else if (cartitems.length > 0) {
+    document.getElementById("contentCart").style.display = "none";
+    document.getElementById("cart").style.display = "block";
+  }
 }
 
 function displaycartitems() {
-    var cartitems = JSON.parse(localStorage.getItem("cartitems")) || [];
-    var cartList = document.getElementById("cart-items");
-    var cartTotal = document.getElementById("cart-total");
-    var cartConfirm = document.getElementById("cart-confirm");
+  var cartitems = JSON.parse(localStorage.getItem("cartitems")) || [];
+  var cartList = document.getElementById("cart-items");
+  var cartTotal = document.getElementById("cart-total");
+  var cartConfirm = document.getElementById("cart-confirm");
 
-    cartList.innerHTML = "";
+  cartList.innerHTML = "";
 
-    // Loop through each item in the cart
-    cartitems.forEach((item) => {
-        // Create HTML elements for the item
-        var itemImage = document.createElement("img");
-        var itemName = document.createElement("span");
-        var itemPrice = document.createElement("span");
-        var itemQuantity = document.createElement("input");
-        var listItem = document.createElement("li");
-        var itemInfo = document.createElement("div");
-        var itemActions = document.createElement("div");
-        var itemTotal = document.createElement("span");
-        var deleteButton = document.createElement("button");
+  // Loop through each item in the cart
+  cartitems.forEach((item) => {
+    // Create HTML elements for the item
+    var itemImage = document.createElement("img");
+    var itemName = document.createElement("span");
+    var itemPrice = document.createElement("span");
+    var itemQuantity = document.createElement("input");
+    var listItem = document.createElement("li");
+    var itemInfo = document.createElement("div");
+    var itemActions = document.createElement("div");
+    var itemTotal = document.createElement("span");
+    var deleteButton = document.createElement("button");
 
-        // Set attributes and content for the HTML elements
-        itemImage.src = item.image;
-        itemName.textContent = item.name;
-        itemPrice.textContent = item.price;
-        itemQuantity.type = "number";
-        itemQuantity.min = "1";
-        itemQuantity.value = "1";
-        itemTotal.textContent = item.price;
-        deleteButton.textContent = "X";
+    // Set attributes and content for the HTML elements
+    itemImage.src = item.image;
+    itemName.textContent = item.name;
+    itemPrice.textContent = item.price;
+    itemQuantity.type = "number";
+    itemQuantity.min = "1";
+    itemQuantity.value = "1";
+    itemTotal.textContent = item.price;
+    deleteButton.textContent = "X";
 
-        // Add classes to the HTML elements
-        itemInfo.classList.add("item-info");
-        itemName.classList.add("item-name");
-        itemPrice.classList.add("item-price");
-        itemActions.classList.add("item-actions");
-        itemQuantity.classList.add("item-quantity");
-        itemTotal.classList.add("item-total");
+    // Add classes to the HTML elements
+    itemInfo.classList.add("item-info");
+    itemName.classList.add("item-name");
+    itemPrice.classList.add("item-price");
+    itemActions.classList.add("item-actions");
+    itemQuantity.classList.add("item-quantity");
+    itemTotal.classList.add("item-total");
 
-
-        // Add event listeners for the quantity input and delete button
-        itemQuantity.addEventListener("input", () => {
-            var quantityInput = itemQuantity;
-            var quantity = parseInt(quantityInput.value);
-            var price = parseInt(item.price.replace("EGP", "").trim());
-            var totalPrice = quantity * price;
-            itemTotal.textContent = `EGP ${totalPrice.toFixed(2)}`
-            updateCartTotal();
-            console.log(totalPrice);
-        });
-
-        deleteButton.addEventListener("click", () => {
-            var index = cartitems.indexOf(item);
-            var iteeem = cartitems[index];
-            cartitems.splice(index, 1);
-            localStorage.setItem("cartitems", JSON.stringify(cartitems));
-            //
-            var arrayString = localStorage.getItem('AllProducts');
-            var AllProducts = JSON.parse(arrayString);
-            function findObjectsByCriteria(array, criteria) {
-                return array.filter(obj => {
-                    for (let key in criteria) {
-                        if (obj[key] !== criteria[key]) {
-                            return false;
-                        }
-                    }
-                    return true;
-                });
-            }
-            //
-            let foundObjects = findObjectsByCriteria(AllProducts, iteeem);
-            if (foundObjects.length > 0) {
-                alert('Objects found:', foundObjects);
-                debugger
-
-                AllProducts.splice(foundObjects, 1);
-                localStorage.setItem("AllProducts", JSON.stringify(AllProducts));
-            } else {
-                alert('No objects found');
-            }
-            //
-            location.reload()
-            displaycartitems();
-            updateCartTotal();
-
-        });
-
-        // Append the HTML elements to the list item
-        itemInfo.appendChild(itemName);
-        itemInfo.appendChild(itemPrice);
-        itemActions.appendChild(itemQuantity);
-        itemActions.appendChild(itemTotal);
-        itemActions.appendChild(deleteButton);
-        listItem.appendChild(itemImage);
-        listItem.appendChild(itemInfo);
-        listItem.appendChild(itemActions);
-
-        // Append the list item to the cart list
-        cartList.appendChild(listItem);
-
-        // Add event listeners for the quantity input and delete button
-        itemQuantity.addEventListener("input", () => {
-            var quantityInput = itemQuantity;
-            var quantity = parseInt(quantityInput.value);
-            var price = parseInt(item.price.replace("EGP", "").trim());
-            var totalPrice = quantity * price;
-            itemTotal.textContent = `EGP ${totalPrice.toFixed(2)}`;
-            updateCartTotal();
-            console.log(totalPrice);
-
-        });
-
-        deleteButton.addEventListener("click", () => {
-            var index = cartitems.indexOf(item);
-            cartitems.splice(index, 1);
-            localStorage.setItem("cartitems", JSON.stringify(cartitems));
-            location.reload();
-            displaycartitems();
-            updateCartTotal();
-        });
-
-        // Append the HTML elements to the list item
-        itemInfo.appendChild(itemName);
-        itemInfo.appendChild(itemPrice);
-        itemActions.appendChild(itemQuantity);
-        itemActions.appendChild(itemTotal);
-        itemActions.appendChild(deleteButton);
-        listItem.appendChild(itemImage);
-        listItem.appendChild(itemInfo);
-        listItem.appendChild(itemActions);
-
-        // Append the list item to the cart list
-        cartList.appendChild(listItem);
+    // Add event listeners for the quantity input and delete button
+    itemQuantity.addEventListener("input", () => {
+      var quantityInput = itemQuantity;
+      var quantity = parseInt(quantityInput.value);
+      var price = parseInt(item.price.replace("EGP", "").trim());
+      var totalPrice = quantity * price;
+      itemTotal.textContent = `EGP ${totalPrice.toFixed(2)}`;
+      updateCartTotal();
+      console.log(totalPrice);
     });
 
-    updateCartTotal();
+    deleteButton.addEventListener("click", () => {
+      var index = cartitems.indexOf(item);
+      var iteeem = cartitems[index];
+      cartitems.splice(index, 1);
+      localStorage.setItem("cartitems", JSON.stringify(cartitems));
+      //
+      var arrayString = localStorage.getItem("AllProducts");
+      var AllProducts = JSON.parse(arrayString);
+      function findObjectsByCriteria(array, criteria) {
+        return array.filter((obj) => {
+          for (let key in criteria) {
+            if (obj[key] !== criteria[key]) {
+              return false;
+            }
+          }
+          return true;
+        });
+      }
+      //
+      let foundObjects = findObjectsByCriteria(AllProducts, iteeem);
+      if (foundObjects.length > 0) {
+        alert("Objects found:", foundObjects);
+        debugger;
+
+        AllProducts.splice(foundObjects, 1);
+        localStorage.setItem("AllProducts", JSON.stringify(AllProducts));
+      } else {
+        alert("No objects found");
+      }
+      //
+      location.reload();
+      displaycartitems();
+      updateCartTotal();
+    });
+
+    // Append the HTML elements to the list item
+    itemInfo.appendChild(itemName);
+    itemInfo.appendChild(itemPrice);
+    itemActions.appendChild(itemQuantity);
+    itemActions.appendChild(itemTotal);
+    itemActions.appendChild(deleteButton);
+    listItem.appendChild(itemImage);
+    listItem.appendChild(itemInfo);
+    listItem.appendChild(itemActions);
+
+    // Append the list item to the cart list
+    cartList.appendChild(listItem);
+
+    // Add event listeners for the quantity input and delete button
+    itemQuantity.addEventListener("input", () => {
+      var quantityInput = itemQuantity;
+      var quantity = parseInt(quantityInput.value);
+      var price = parseInt(item.price.replace("EGP", "").trim());
+      var totalPrice = quantity * price;
+      itemTotal.textContent = `EGP ${totalPrice.toFixed(2)}`;
+      updateCartTotal();
+      console.log(totalPrice);
+    });
+
+    deleteButton.addEventListener("click", () => {
+      var index = cartitems.indexOf(item);
+      cartitems.splice(index, 1);
+      localStorage.setItem("cartitems", JSON.stringify(cartitems));
+      location.reload();
+      displaycartitems();
+      updateCartTotal();
+    });
+
+    // Append the HTML elements to the list item
+    itemInfo.appendChild(itemName);
+    itemInfo.appendChild(itemPrice);
+    itemActions.appendChild(itemQuantity);
+    itemActions.appendChild(itemTotal);
+    itemActions.appendChild(deleteButton);
+    listItem.appendChild(itemImage);
+    listItem.appendChild(itemInfo);
+    listItem.appendChild(itemActions);
+
+    // Append the list item to the cart list
+    cartList.appendChild(listItem);
+  });
+
+  updateCartTotal();
 }
 
 function updateCartTotal() {
-    var cartitems = JSON.parse(localStorage.getItem("cartitems")) || [];
-    console.log(cartitems);
-    var cartTotal = document.getElementById("cart-total");
-    let total = 0;
+  var cartitems = JSON.parse(localStorage.getItem("cartitems")) || [];
+  console.log(cartitems);
+  var cartTotal = document.getElementById("cart-total");
+  let total = 0;
 
-    // Loop through each item in the cart and add up the total price
-    cartitems.forEach((item) => {
-        var quantity = parseInt(item.quantity) || 1;
+  // Loop through each item in the cart and add up the total price
+  cartitems.forEach((item) => {
+    var quantity = parseInt(item.quantity) || 1;
 
-        var price = parseInt(item.price.replace("EGP", "").trim());
-        var totalPrice = quantity * price;
-        total += totalPrice;
-        cartTotal.textContent = `Total: EGP ${total.toFixed(2)}`;
-    });
+    var price = parseInt(item.price.replace("EGP", "").trim());
+    var totalPrice = quantity * price;
+    total += totalPrice;
+    cartTotal.textContent = `Total: EGP ${total.toFixed(2)}`;
+  });
 }
 displaycartitems();
 
 //checkout button
 document.getElementById("checkoutBtn").onclick = function () {
-    document.getElementById("checkoutPage").style.display = "flex";
+  document.getElementById("checkoutPage").style.display = "flex";
 };
 
 document.getElementById("close").onclick = function () {
-    document.getElementById("checkoutPage").style.display = "none";
+  document.getElementById("checkoutPage").style.display = "none";
 };
 // function checkLocalStorage() {
 //     var user = localStorage.getItem('users');
